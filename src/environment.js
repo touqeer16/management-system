2c9bb3fce03bb4895d4af64a944f0e924fdf4e39
Touqeer Fazal <touqeerfazal1992@gmail.com>
2025-07-18 01:01:26 +0300

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,4 @@
 export const environment = {
     URL: window.location.protocol + '//' + window.location.hostname + ":8000",
-    //URL: 'https://staging.fastich.tech',
-   // URL: 'https://panel.fastich.tech',
+
 }
